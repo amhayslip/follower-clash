777c14d817a541c88d1cb77b908173b94be0ce95
amhayslip <amhayslip@gmail.com>
2013-11-26 20:50:07 -0600

    Added more methods to extract more data from the twitter api

--- follower_clash.rb
+++ follower_clash.rb
@@ -2,9 +2,22 @@ require 'rubygems'
 require 'bundler/setup'
 require_relative 'lib/user_comparer'
 
-user1 = UserComparer::User.new('ornellasmike')
-user2 = UserComparer::User.new('techpeace')
+user1 = UserComparer::User.new('aaronhayslip')
+user2 = UserComparer::User.new('justinbieber')
 puts "#{user1.username}'s follower count: #{user1.followers}"
 puts "#{user2.username}'s follower count: #{user2.followers}"
 comparer = UserComparer::Comparer.new(user1, user2)
+puts "#{user1.username}'s friend count: #{user1.friends}"
+puts "#{user2.username}'s friend count: #{user2.friends}"
+puts "#{user1.username}'s tweet count: #{user1.tweets}"
+puts "#{user2.username}'s tweet count: #{user2.tweets}"
+puts "#{user1.username}'s favorite count: #{user1.lists}"
+puts "#{user2.username}'s favorite count: #{user2.lists}"
+puts "#{user1.username} is from #{user1.location}"
+puts "#{user2.username} is from #{user2.location}"
+puts "#{user1.username}'s last tweet says: #{user1.status}"
+puts "#{user2.username}'s last tweet says: #{user2.status}"
 puts "And the user with the most followers is: #{comparer.compare}"
+puts "And the user with the most friends is: #{comparer.comp_friends}"
+puts "And the user with the most tweets is: #{comparer.comp_tweets}"
+puts "And the user with the most lists is: #{comparer.comp_lists}"
--- lib/user_comparer.rb
+++ lib/user_comparer.rb
@@ -7,18 +7,37 @@ module UserComparer
   	attr_reader :username
   	def initialize(username)
   		@username = username
+      @client = Twitter::REST::Client.new do |config|
+        config.consumer_key        = ENV['CONSUMER_KEY']
+        config.consumer_secret     = ENV['CONSUMER_SECRET']
+        config.access_token         = ENV['OAUTH_TOKEN']
+        config.access_token_secret  = ENV['OAUTH_TOKEN_SECRET']
+      end
   	end
 
   	def followers
-  	  client = Twitter::REST::Client.new do |config|
-  	    config.consumer_key        = ENV['CONSUMER_KEY']
-  	    config.consumer_secret     = ENV['CONSUMER_SECRET']
-  	    config.access_token         = ENV['OAUTH_TOKEN']
-  	    config.access_token_secret  = ENV['OAUTH_TOKEN_SECRET']
-  	  end
-
-  	  client.user(@username).followers_count
+  	  @client.user(@username).followers_count
   	end
+
+  	def friends
+  	  @client.user(@username).friends_count
+  	end
+
+  	def tweets
+  	  @client.user(@username).statuses_count
+  	end
+
+    def lists
+      @client.user(@username).listed_count
+    end
+
+    def location
+      @client.user(@username).location
+    end
+
+    def status
+      @client.user(@username).status.text
+    end
 
   end
 
@@ -31,13 +50,46 @@ module UserComparer
   	def compare
   		# returns user with more followers, or both if even
   		if @user1.followers > @user2.followers 
-  			return "#{@user1.username} has more followers!"
+  			return  @user1.username
   		elsif @user1.followers < @user2.followers
-  			return "#{@user2.username} has more followers!"
+  			return @user2.username
   		else
   			return "#{@user1.username} and #{@user2.username} have the same number of followers!  Who knew!?"
   		end
   	end
+
+  	def comp_friends
+  		if @user1.friends > @user2.friends
+  			return  @user1.username
+  		elsif @user1.friends < @user2.friends
+  			return @user2.username
+  		else
+  			return "#{@user1.username} and #{@user2.username} have the same number of friends!  Who knew!?"
+  		end
+  	end
+
+  	def comp_tweets
+  		if @user1.tweets > @user2.tweets
+  			return  @user1.username
+  		elsif @user1.tweets < @user2.tweets
+  			return @user2.username
+  		else
+  			return "#{@user1.username} and #{@user2.username} have the same number of tweets!  Who knew!?"
+  		end
+  	end
+
+    def comp_lists
+      if @user1.lists > @user2.lists
+        return  @user1.username
+      elsif @user1.lists < @user2.lists
+        return @user2.username
+      else
+        return "#{@user1.username} and #{@user2.username} have the same number of favorites!  Who knew!?"
+      end
+    end
+
+
+  	
 
   end
 end
